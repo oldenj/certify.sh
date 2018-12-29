4963e7c37eb7b640e8330461edf765bbf1c5449a
Jurek Olden <jurek.olden@in.tum.de>
2018-12-29 16:25:24 +0100

    change default pki dir to /etc/certify/letsencryp

--- certify.sh
+++ certify.sh
@@ -15,7 +15,7 @@ RENEW=false
 STAGING=false
 DOMAIN=""
 ALT_DOMAINS=()
-PKI_DIR="/data/pki/letsencrypt"
+PKI_DIR="/etc/certify/letsencrypt"
 ACME_DIR="/srv/http/acme-challenge/.well-known/acme-challenge/"
 
 # Colors for logging
